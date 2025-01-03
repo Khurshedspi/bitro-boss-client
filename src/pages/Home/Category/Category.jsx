f7f0976a0f334a8d7cb9dd7a601ea9aae279019a
Khurshedspi <khurshed5322@gmail.com>
2025-01-03 20:47:27 +0600

    setup route system and install that need for Bitro Boss

---
+++ src/pages/Home/Category/Category.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Category = () => {
+    return (
+        <div>
+            <h3>Category here</h3>
+        </div>
+    );
+};
+
+export default Category;
---
+++ src/pages/Home/Featured/Featured.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Featured = () => {
+    return (
+        <div>
+            <h3>featured here </h3>
+        </div>
+    );
+};
+
+export default Featured;
